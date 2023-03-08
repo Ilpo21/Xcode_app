1a7007875c60121e3855dd5c17f21ea8fde3d2f2
ilpo21 <ilarikeronen@gmail.com>
2023-03-08 10:58:31 +0200

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Ilari Keronen on 8.3.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
